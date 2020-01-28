808b6beede92e1bedd96b6243998a0e6ea8b4866
Vladimir Barov <vladimir.barov404@gmail.com>
2020-01-29 00:23:53 +0300

    add output image scale parameter

--- ffmpegExample/Program.cs
+++ ffmpegExample/Program.cs
@@ -12,7 +12,7 @@ namespace ffmpegExample
             startInfo.WindowStyle = ProcessWindowStyle.Hidden;
 
             startInfo.FileName = @"C:\Windows\System32\cmd.exe";
-            startInfo.Arguments = $@"/c E:\Downloads\ffmpeg-4.2.2-win64-static\ffmpeg-4.2.2-win64-static\bin\ffmpeg.exe -i E:\Downloads\big_buck_bunny_720p_5mb.mp4 img%03d.bmp";
+            startInfo.Arguments = $@"/c E:\Downloads\ffmpeg-4.2.2-win64-static\ffmpeg-4.2.2-win64-static\bin\ffmpeg.exe -i E:\Downloads\big_buck_bunny_720p_5mb.mp4 -vf scale=320:240 img%03d.bmp";
             process.StartInfo = startInfo;
             process.Start();
             process.WaitForExit();
